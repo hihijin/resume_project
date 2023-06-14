8ee597018aa96b1e1b7d824d8b315fdca950c3c1
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-14 18:26:49 +0900

    [Feat]: Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { lazy, Suspense } from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Loading from "./pages/Loading";
+
+const MainPage = lazy(() => import("./pages/MainPage"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Suspense>
+      <Footer />
     </div>
   );
 }

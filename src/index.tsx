e5d4cd82439adf7c73d13a661f8d87781aa86ba2
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-15 17:12:19 +0900

    [Feat] : 스크롤탑 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
+import ScrollToTop from "./util/useScrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );

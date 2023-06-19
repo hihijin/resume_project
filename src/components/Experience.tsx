220322136848279af17bcc424851dd9b11025209
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-19 15:16:58 +0900

    [Design] : 구현 화면 이미지 추가

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,10 +4,13 @@ import styled from "styled-components";
 
 import feedback from "../assets/feedback.png";
 import logo from "../assets/logo.png";
+import mbti1 from "../assets/mbti1.png";
+import mbti2 from "../assets/mbti2.png";
+import mbti3 from "../assets/mbti3.png";
+import mbti4 from "../assets/mbti4.png";
 
 const Main = styled.div`
   width: 100%;
-  padding: 30px 0;
   margin-bottom: 100px;
   .title {
     color: rgba(0, 0, 0, 0.8);
@@ -62,7 +65,6 @@ const Main = styled.div`
   .feedback {
     width: 600px;
     height: auto;
-    margin-bottom: 100px;
     @media (max-width: 645px) {
       width: 500px;
     }
@@ -75,6 +77,36 @@ const Main = styled.div`
   }
   .noborder {
     border: none;
+    margin-top: 20px;
+  }
+  .blue {
+    background: #dff6ff;
+  }
+`;
+
+const ImgBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: baseline;
+  padding: 20px 0;
+  border-bottom: 0.5px solid rgba(0, 0, 0, 0.1);
+  @media (max-width: 1012px) {
+    flex-flow: wrap;
+  }
+  img {
+    width: 250px;
+    height: 200px;
+    margin-right: 10px;
+    @media (max-width: 547px) {
+      width: 180px;
+      height: 130px;
+      margin-right: 5px;
+    }
+    @media (max-width: 389px) {
+      width: 130px;
+      height: 80px;
+    }
   }
 `;
 
@@ -136,19 +168,31 @@ function Experience() {
       >
         프로젝트 회고 링크
       </button>
+      <ImgBox>
+        <img src={mbti1} alt="구현화면" />
+        <img src={mbti2} alt="구현화면" />
+        <img src={mbti3} alt="구현화면" />
+        <img src={mbti4} alt="구현화면" />
+      </ImgBox>
       <div className="subTitle">🪧프로젝트 설명</div>
       <div className="text">
-        MBTI기반 여행지 추천&커뮤니티 서비스 <br />
+        <span className="blue">MBTI기반 여행지 추천&커뮤니티 서비스</span>{" "}
+        <br />
         최근 한국인의 해외여행 관광비율에 비해 적은 국내여행 관광 비율의 문제를
         인식하고,
         <br />
         국내 유명명소들을 소개하여 국내 관광시장의 활기를 되찾기 위해 MZ세대에서
-        현재 대유행하고 있는 “MBTI”키워드를 접목하여 각 MBTI별 여행지 추천
-        서비스와 함께 여행커뮤니티 서비스 기획
+        현재 대유행하고 있는 <span className="blue">MBTI</span>키워드를 접목하여
+        각 MBTI별 여행지 추천 서비스와 함께 여행커뮤니티 서비스 기획
         <br />
         <br />
-        팀원 - 프론트엔드 개발자 3명, 백엔드 개발자 3명 <br />
-        프로젝트 기간 - 2023.04.28 ~ 2023.05.25 (약 1개월)
+        팀원 -{" "}
+        <span className="blue">
+          프론트엔드 개발자 3명, 백엔드 개발자 3명
+        </span>{" "}
+        <br />
+        프로젝트 기간 -{" "}
+        <span className="blue">2023.04.28 ~ 2023.05.25 (약 1개월)</span>
       </div>
       <div className="subTitle">⚒️기술 스택</div>
       <div className="text">
@@ -180,22 +224,42 @@ function Experience() {
         <li>AWS s3, cloudFront, Route53를 활용하여 https로 배포</li>
       </ul>
       <div className="subTitle">🔥성장 경험</div>
-      <div className="text noborder">
-        프로젝트에서 팀장 역할을 맡아 회의 주도, 문서 및 일정 관리를 담당했고,
-        팀 내 의사소통을 적극적으로 이끌어가며 소외되는 팀원이 없도록 했습니다.{" "}
-        <br />
+      <div className="text">
+        프로젝트에서 <span className="blue">팀장</span> 역할을 맡아 회의 주도,
+        문서 및 일정 관리를 담당했고, 팀 내{" "}
+        <span className="blue">의사소통을 적극적</span>으로 이끌어가며 소외되는
+        팀원이 없도록 했습니다. <br />
         회의 및 토의 시에는 모든 팀원들의 의견을 듣고 공정한 의사결정을 내리기
         위해 노력했습니다. 열린 마음으로 피드백을 주고받으며 팀원들의 의견을
-        수렴하며 효율적인 협업환경을 조성했습니다.
+        수렴하며 <span className="blue">효율적인 협업환경</span>을 조성했습니다.
         <br />
         <br />
-        또한, 코드 리뷰를 통해 컴포넌트 분리를 중점으로 개선점을 찾고 코드의
-        재사용성과 유지보수성을 고려하여 모듈화된 컴포넌트를 설계하고 구현하는
-        데 신경을 썼습니다. 이를 통해 코드의 가독성과 유연성을 향상시키며,
-        전체적인 프로젝트의 효율성을 높이는 데 기여했습니다.
+        <span className="blue">코드 리뷰</span>를 통해{" "}
+        <span className="blue">컴포넌트 분리</span>를 중점으로 개선점을 찾고
+        코드의 재사용성과 유지보수성을 고려하여{" "}
+        <span className="blue">모듈화</span>된 컴포넌트를 설계하고 구현하는 데
+        신경을 썼습니다. 이를 통해{" "}
+        <span className="blue">코드의 가독성과 유연성</span>을 향상시키며,
+        전체적인 프로젝트의 <span className="blue">효율성</span>을 높이는 데
+        기여했습니다.
+      </div>
+      <div className="subTitle">👩‍💻Problem</div>
+      <div className="text">
+        프로젝트에서 HTTP로 배포된 사이트를 oauth로 인해 급하게{" "}
+        <span className="blue">HTTPS</span>로 변경해야 하는{" "}
+        <span className="blue">문제</span>가 있었습니다.
         <br />
-        <br />
-        *해당 프로젝트 종료 후 팀원분들이 작성해주신 피드백과 후기입니다.
+        원래 배포 담당 팀원의 도움요청으로 제가 마감 3일 전에 새롭게 HTTPS
+        배포를 완료 후 Mixed Content 오류가 발생하였지만, Backend 팀원과
+        협력하여 마감 2일 전 서버까지 HTTPS 배포로 오류를 해결하면서 일정을
+        준수하였습니다. <br />이 경험을 통해 앞으로의 프로젝트에서는 배포 일정을
+        미리 계획하고 여유롭게 설정하며,{" "}
+        <span className="blue">문제 해결에 적극적인 자세</span>를 가지고{" "}
+        <span className="blue"> 팀원들과 협력</span>하여 효과적인 프로젝트
+        진행에 기여하고자 합니다.
+      </div>
+      <div className="text noborder">
+        *해당 프로젝트 종료 후 팀원분들이 작성해주신 피드백과 후기입니다.*
       </div>
       <img className="feedback" src={feedback} alt="feedback" />
     </Main>
